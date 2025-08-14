e3dabb17dd8f63eaf7c95621ec7b880da5743c6b
clark <clark.hobbie@gmail.com>
2025-08-14 17:30:15 -0600

    fixed SaveServletTest and PropertiesHolderTest

--- src/test/java/com/ltsllc/miranda/properties/PropertiesHolderTest.java
+++ src/test/java/com/ltsllc/miranda/properties/PropertiesHolderTest.java
@@ -71,7 +71,7 @@ public class PropertiesHolderTest {
         String table[][] = {
                 {Miranda.PROPERTY_CACHE_LOAD_LIMIT, Miranda.PROPERTY_DEFAULT_CACHE_LOAD_LIMIT},
                 {Miranda.PROPERTY_CLUSTER, "on"},
-                {Miranda.PROPERTY_CLUSTER_1, "71.237.68.250"},
+                {Miranda.PROPERTY_CLUSTER_1, "10.0.0.49"},
                 {Miranda.PROPERTY_USE_HEARTBEATS, Miranda.PROPERTY_DEFAULT_USE_HEART_BEATS},
                 {Miranda.PROPERTY_MESSAGE_LOG, Miranda.PROPERTY_DEFAULT_MESSAGE_LOG},
                 {Miranda.PROPERTY_BID_TIMEOUT, Miranda.PROPERTY_DEFAULT_BID_TIMEOUT},
@@ -80,7 +80,7 @@ public class PropertiesHolderTest {
                 {Miranda.PROPERTY_COMPACTION_TIME, Miranda.PROPERTY_DEFAULT_COMPACTION_TIME},
                 {Miranda.PROPERTY_THIS_PORT, Miranda.PROPERTY_DEFAULT_CLUSTER_PORT},
                 {Miranda.PROPERTY_SCAN_PERIOD, Miranda.PROPERTY_DEFAULT_SCAN_PERIOD},
-                {Miranda.PROPERTY_THIS_HOST, "10.0.0.49"},
+                {Miranda.PROPERTY_THIS_HOST, "10.0.0.236"},
                 {Miranda.PROPERTY_HEART_BEAT_INTERVAL, Miranda.PROPERTY_DEFAULT_HEART_BEAT_INTERVAL},
                 {Miranda.PROPERTY_CLUSTER_PORT, Miranda.PROPERTY_DEFAULT_CLUSTER_PORT},
                 {Miranda.PROPERTY_DEAD_NODE_TIMEOUT, Miranda.PROPERTY_DEFAULT_DEAD_NODE_TIMEOUT},
